42e4b0e3ee722ec2a3d8017a2d5c11239c34b891
rfsyhb <rafisyihab3@gmail.com>
2025-12-15 21:21:20 +0700

    base data

---
+++ src/lib/data.ts
@@ -0,0 +1,24 @@
+export const farmData = {
+  'Riverwort Village Ruins': {
+    runDuration: 22,
+    defaultGoldEarned: 20,
+  },
+  'Dragon Follower Base': {
+    runDuration: 22,
+    defaultGoldEarned: 22,
+  },
+  'Ancient Library': {
+    runDuration: 32,
+    defaultGoldEarned: 20,
+  },
+  'East Ancient Armory': {
+    runDuration: 34,
+    defaultGoldEarned: 20,
+  },
+  'West Ancient Armory': {
+    runDuration: 27,
+    defaultGoldEarned: 20,
+  },
+};
+
+export const goldRate = 9200; // rupiah per 100 gold
